6e98011e2e05c3816e8d10f3f99cb41b7899c8b5
jesuscriss301 <jesusmanuelcp@ufps.edu.co>
2022-04-29 00:51:01 -0500

    funcionalidad noticia 1, pequeños arreglos

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -7,7 +7,7 @@ function cargarNoticias()
 
 	let articulo= "<article class='' >"+"\n"+
             " <h3>titulo - categoria- fecha</h3>"+"\n"+
-            " <p>@@. <a href='#'>ver más</a></p> "+"\n"+
+            " <p>@@. <a href='#' name='post' title='ide'>ver más</a> </p> "+"\n"+
             "</article>"+"\n";
 
 
@@ -17,22 +17,21 @@ function cargarNoticias()
 		for (i=0; i < 3; i++) {
 
 			//if (i==response.length) {break;}
-			if (response[i].id == "1") {
+			if (response[i].id == 1) {
 				articulo=articulo.replace("#","html/noticias.html");
-				articulo+="<form class='' action='html/noticias.html' method='post'>"+"\n"+
-				  				"  <input type='hidden' name='id' value='1'>"+"\n"+
-									"</form>"+"\n"+
+
 			}
 			articulos+=articulo.replace("titulo",response[i].titulo).
 							replace("categoria",response[i].categoria).
 							replace("fecha",response[i].fecha).
-							replace("@@",response[i].descripcion);
+							replace("@@",response[i].descripcion).
+							replace("ide",response[i].id);
 
 		}
 
 		articulos=div+articulos+"\n</div>";
 		document.getElementById("noticias-principales").innerHTML = articulos;
-    console.log(articulos);
+
 
 		//pagination(response);
 	});
@@ -59,7 +58,7 @@ function deportes(){
 
     articulos=div+articulos+"\n</div>";
     document.getElementById("deporte").innerHTML = articulos;
-    console.log(articulos);
+
 
     //pagination(response);
   });
@@ -86,7 +85,35 @@ function tecnologias(){
 
     articulos=div+articulos+"\n</div>";
     document.getElementById("tecnologia").innerHTML = articulos;
-    console.log(articulos);
+
+
+    //pagination(response);
+  });
+
+}
+function cargarNoticia1(){
+  let url="https://carlosreneas.github.io/endpoints/noticia_1.json";
+  let articulos="";
+  let div="<div class=''>"+"\n";
+  let articulo= "<article class='' >"+"\n"+
+							"<img src='imagen' alt=''>"+"\n"+
+            " <h3>titulo - categoria- fecha</h3>"+"\n"+
+            " <p>descripcion. </p> "+"\n"+
+						" <p>detalle. </p> "+"\n"+
+            "</article>"+"\n";
+
+
+  fetch(url, {method:"GET", headers: { origin:"dominio.com" }}).then(response=>response.json()).then(response=>{
+        articulos+=articulo.replace("titulo",response.titulo).
+								replace("categoria",response.categoria).
+								replace("fecha",response.fecha).
+								replace("descripcion",response.descripcion).
+								replace("detalle",response.detalle).
+								replace("imagen",response.img);
+
+
+    articulos=div+articulos+"\n</div>";
+    document.getElementById("noticia1").innerHTML = articulos;
 
     //pagination(response);
   });
