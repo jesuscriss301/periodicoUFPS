57fb3e2fabe618b056d4f0b17bf16cdd9b2bcfd9
jesuscriss301 <jesusmanuelcp@ufps.edu.co>
2022-04-28 20:24:43 -0500

    tablillas de deportes y tecnologias

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -1,3 +1,4 @@
+let cantidadNoticias= 3;
 function cargarNoticias()
 {
 	let url="https://carlosreneas.github.io/endpoints/noticias.json";
@@ -33,28 +34,51 @@ function cargarNoticias()
 function deportes(){
   let url="https://carlosreneas.github.io/endpoints/categoria_deporte.json";
   let articulos="";
-  let div="<div class=''>"+"\n";
+  let div="<div class=''>"+"\n"+
+            "<h3>Deportes</h3>"+"\n";
 
-  let articulo= "<article class='' >"+"\n"+
-            " <h3>titulo - categoria- fecha</h3>"+"\n"+
-            " <p>@@. <a href='#'>ver más</a></p> "+"\n"+
-            "</article>"+"\n";
+  let articulo= "<a href='#'><p>descripcion</p></a>"
 
 
   fetch(url, {method:"GET", headers: { origin:"dominio.com" }}).then(response=>response.json()).then(response=>{
 
 
-    for (i=0; i < 3; i++) {
+    for (i=0; i < response.length; i++) {
 
-      //if (i==response.length) {break;}
-      articulos+=articulo.replaceAll("titulo",response[i].titulo).
-              replaceAll("categoria",response[i].categoria).
-              replaceAll("fecha",response[i].fecha).
-              replaceAll("@@",response[i].descripcion);
+      if (response[i].categoria=="deporte") {
+        articulos+=articulo.replaceAll("descripcion",response[i].titulo);
+      }
     }
 
     articulos=div+articulos+"\n</div>";
-    document.getElementById("noticias-principales").innerHTML = articulos;
+    document.getElementById("deporte").innerHTML = articulos;
+    console.log(articulos);
+
+    //pagination(response);
+  });
+
+}
+function tecnologias(){
+  let url="https://carlosreneas.github.io/endpoints/categoria_tecnologia.json";
+  let articulos="";
+  let div="<div class=''>"+"\n"+
+            "<h3>Tecnologís</h3>"+"\n";
+
+  let articulo= "<a href='#'><p>descripcion</p></a>"
+
+
+  fetch(url, {method:"GET", headers: { origin:"dominio.com" }}).then(response=>response.json()).then(response=>{
+
+
+    for (i=0; i < response.length; i++) {
+
+      if (response[i].categoria=="tecnologia") {
+        articulos+=articulo.replaceAll("descripcion",response[i].titulo);
+      }
+    }
+
+    articulos=div+articulos+"\n</div>";
+    document.getElementById("tecnologia").innerHTML = articulos;
     console.log(articulos);
 
     //pagination(response);
